c671ae9249a795c235d48638dee120874a101e56
marina saraiva <marinasaraiva@hotmail.fr>
2025-07-07 18:22:40 +0200

    feat: finish !

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -3,8 +3,24 @@ import "../styles/intro.css";
 import photo from "../Marina_Saraiva.png";
 import github from "../images/github-white-icon.svg";
 import linkedin from "../images/linkedin-white-icon.svg";
+import { useState } from "react";
+
 
 function Intro() {
+      const [open, setOpen] = useState(false);
+  
+      const scrollToSection = (id: string) => {
+          const element = document.getElementById(id);
+          if (element) {
+                if (id === "projets") {
+                  const offset = -150;
+                  const y = element.getBoundingClientRect().top + window.pageYOffset + offset;
+                  window.scrollTo({ top: y, behavior: "smooth" });
+              } else {
+                  element.scrollIntoView({ behavior: "smooth" });
+              }
+          }
+      };
 
     return(
       <>
@@ -14,7 +30,7 @@ function Intro() {
           <h1 className="hello">Marina Saraiva </h1>
           <h2 className="color-dev">Développeuse Web.</h2>
           <p className="description">Passionnée par la conception, motivée à apprendre.</p>
-          <button className="button-projet">Voir mes projets</button>
+          <button className="button-projet" onClick={() => scrollToSection("projets")}>Voir mes projets</button>
         </div>  
         <div>
           <div className="picture-intro">
